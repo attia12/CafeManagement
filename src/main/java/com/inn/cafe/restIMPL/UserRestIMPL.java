6825c13949b250926f710706ef720c3df2049bbd
ahmedkarou <ahmed.karou@esprit.tn>
2023-05-14 20:29:36 +0100

    classes controller and services

---
+++ src/main/java/com/inn/cafe/restIMPL/UserRestIMPL.java
@@ -0,0 +1,9 @@
+package com.inn.cafe.restIMPL;
+
+import com.inn.cafe.rest.UserRest;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserRestIMPL implements UserRest {
+}
---
+++ src/main/java/com/inn/cafe/serviceIMPL/UserServiceIMPL.java
@@ -0,0 +1,9 @@
+package com.inn.cafe.serviceIMPL;
+
+import com.inn.cafe.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class UserServiceIMPL implements UserService {
+}
