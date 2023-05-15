d0922d2a8ba0bd5aaf2732a4550b9f2975b26f59
ahmedkarou <ahmed.karou@esprit.tn>
2023-05-15 05:41:33 +0100

    signUp APi work with validation

--- src/main/java/com/inn/cafe/dao/UserDAO.java
+++ src/main/java/com/inn/cafe/dao/UserDAO.java
@@ -2,6 +2,8 @@ package com.inn.cafe.dao;
 
 import com.inn.cafe.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserDAO extends JpaRepository<User,Integer> {
+    User findByEmailId(@Param("email") String email);
 }
--- src/main/java/com/inn/cafe/restIMPL/UserRestIMPL.java
+++ src/main/java/com/inn/cafe/restIMPL/UserRestIMPL.java
@@ -1,9 +1,32 @@
 package com.inn.cafe.restIMPL;
 
+import com.inn.cafe.constents.CafeConstants;
 import com.inn.cafe.rest.UserRest;
+import com.inn.cafe.service.UserService;
+import com.inn.cafe.utils.CafeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 
 public class UserRestIMPL implements UserRest {
+    @Autowired
+    UserService userService;
+    @Override
+    public ResponseEntity<String> signUP(Map<String, String> requestMapping) {
+        try
+        {
+            return userService.signUp(requestMapping);
+
+        }catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+          return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
 }
